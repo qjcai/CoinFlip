74c82ee5e9502ee75293f67c81cc28763440dbe6
KAI <qcai1997@gmail.com>
2024-05-30 03:40:38 -0400

    Add project files.

---
+++ CoinFlip/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CoinFlip
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FlipButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FlipCountTextBox = new System.Windows.Forms.TextBox();
+            this.HighestHeadLabel = new System.Windows.Forms.Label();
+            this.HighestTailsLabel = new System.Windows.Forms.Label();
+            this.NewHeadRecordLabel = new System.Windows.Forms.Label();
+            this.OldHeadRecordLabel = new System.Windows.Forms.Label();
+            this.NewTailRecordLabel = new System.Windows.Forms.Label();
+            this.OldTailRecordLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FlipButton
+            // 
+            this.FlipButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.FlipButton.Location = new System.Drawing.Point(300, 203);
+            this.FlipButton.Name = "FlipButton";
+            this.FlipButton.Size = new System.Drawing.Size(193, 50);
+            this.FlipButton.TabIndex = 0;
+            this.FlipButton.Text = "Flip The Coin!";
+            this.FlipButton.UseVisualStyleBackColor = true;
+            this.FlipButton.Click += new System.EventHandler(this.FlipButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(184, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "How many times you want to flip? (0 - 1000)";
+            // 
+            // FlipCountTextBox
+            // 
+            this.FlipCountTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.FlipCountTextBox.Location = new System.Drawing.Point(344, 141);
+            this.FlipCountTextBox.Name = "FlipCountTextBox";
+            this.FlipCountTextBox.Size = new System.Drawing.Size(100, 38);
+            this.FlipCountTextBox.TabIndex = 2;
+            // 
+            // HighestHeadLabel
+            // 
+            this.HighestHeadLabel.AutoSize = true;
+            this.HighestHeadLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HighestHeadLabel.Location = new System.Drawing.Point(102, 287);
+            this.HighestHeadLabel.Name = "HighestHeadLabel";
+            this.HighestHeadLabel.Size = new System.Drawing.Size(0, 26);
+            this.HighestHeadLabel.TabIndex = 1;
+            // 
+            // HighestTailsLabel
+            // 
+            this.HighestTailsLabel.AutoSize = true;
+            this.HighestTailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.HighestTailsLabel.Location = new System.Drawing.Point(102, 324);
+            this.HighestTailsLabel.Name = "HighestTailsLabel";
+            this.HighestTailsLabel.Size = new System.Drawing.Size(0, 26);
+            this.HighestTailsLabel.TabIndex = 1;
+            // 
+            // NewHeadRecordLabel
+            // 
+            this.NewHeadRecordLabel.AutoSize = true;
+            this.NewHeadRecordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.NewHeadRecordLabel.Location = new System.Drawing.Point(102, 371);
+            this.NewHeadRecordLabel.Name = "NewHeadRecordLabel";
+            this.NewHeadRecordLabel.Size = new System.Drawing.Size(0, 26);
+            this.NewHeadRecordLabel.TabIndex = 1;
+            // 
+            // OldHeadRecordLabel
+            // 
+            this.OldHeadRecordLabel.AutoSize = true;
+            this.OldHeadRecordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.OldHeadRecordLabel.Location = new System.Drawing.Point(102, 408);
+            this.OldHeadRecordLabel.Name = "OldHeadRecordLabel";
+            this.OldHeadRecordLabel.Size = new System.Drawing.Size(0, 26);
+            this.OldHeadRecordLabel.TabIndex = 1;
+            // 
+            // NewTailRecordLabel
+            // 
+            this.NewTailRecordLabel.AutoSize = true;
+            this.NewTailRecordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.NewTailRecordLabel.Location = new System.Drawing.Point(102, 453);
+            this.NewTailRecordLabel.Name = "NewTailRecordLabel";
+            this.NewTailRecordLabel.Size = new System.Drawing.Size(0, 26);
+            this.NewTailRecordLabel.TabIndex = 1;
+            // 
+            // OldTailRecordLabel
+            // 
+            this.OldTailRecordLabel.AutoSize = true;
+            this.OldTailRecordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.OldTailRecordLabel.Location = new System.Drawing.Point(102, 490);
+            this.OldTailRecordLabel.Name = "OldTailRecordLabel";
+            this.OldTailRecordLabel.Size = new System.Drawing.Size(0, 26);
+            this.OldTailRecordLabel.TabIndex = 1;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.ExitButton.Location = new System.Drawing.Point(546, 203);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 50);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 555);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.FlipCountTextBox);
+            this.Controls.Add(this.OldTailRecordLabel);
+            this.Controls.Add(this.NewTailRecordLabel);
+            this.Controls.Add(this.OldHeadRecordLabel);
+            this.Controls.Add(this.NewHeadRecordLabel);
+            this.Controls.Add(this.HighestTailsLabel);
+            this.Controls.Add(this.HighestHeadLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FlipButton);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Flip The Coin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FlipButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FlipCountTextBox;
+        private System.Windows.Forms.Label HighestHeadLabel;
+        private System.Windows.Forms.Label HighestTailsLabel;
+        private System.Windows.Forms.Label NewHeadRecordLabel;
+        private System.Windows.Forms.Label OldHeadRecordLabel;
+        private System.Windows.Forms.Label NewTailRecordLabel;
+        private System.Windows.Forms.Label OldTailRecordLabel;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ CoinFlip/MainForm.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Program Name: Coin Flip
+ * How to use: User can input the amount of coin flips they desire but from the range of 1 to 1000
+ *             Then the program displays the consecutive heads and tails user roll this turn.
+ *             The program will make a record.txt file to store scores if file have not already exist with the default value of 0s to start with.
+ *             Then the program will check the high score for consecutive heads or tails.
+ *             If the new consecutive is higher than the old consecutive the program will update the file with the new scores
+ *             The program will also let user know if they set a new record or not.
+ */
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CoinFlip
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Disallowing resize
+            this.MaximizeBox = false; // Disallowing maximize
+        }
+
+        private void MakeFile(string filePath)
+        {
+            // Check if file exist, if not, then make file, else then skip
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("0");
+                    writer.WriteLine("0");
+                }
+            }
+        }
+
+        private void FlipButton_Click(object sender, EventArgs e)
+        {
+            // Value init
+            int Head = 1;
+            int Tail = 2;
+            int HeadAmount = 0;
+            int TailAmount = 0;
+            int FlipCount;
+
+            // Input validation making sure its a number between 1 to 1000
+            try
+            {
+                FlipCount = int.Parse(FlipCountTextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show(this, "Amount of Flips must be a number!", "Error");
+                return;
+            }
+
+            if (FlipCount < 1)
+            {
+                MessageBox.Show(this, "Amount of Flips Can not be lower than 1!", "Error");
+                return;
+            }
+            if (FlipCount > 1000)
+            {
+                MessageBox.Show(this, "Amount of Flips Can not be over 1000!", "Error");
+                return;
+            }
+
+            // Random number generator, that generates 1 or 2
+            Random random = new Random();
+
+            for (int i = 0; i < FlipCount; i++) 
+            {
+                int randomNumber = random.Next(1, 3);
+                if (randomNumber == Head) 
+                {
+                    HeadAmount++;
+                } else if (randomNumber == Tail)
+                {
+                    TailAmount++;
+                }
+            }
+
+            // Set label text to current scores
+            HighestHeadLabel.Text = "Number of Consecutive Heads: " + HeadAmount;
+            HighestTailsLabel.Text = "Number of Consecutive Tails: " + TailAmount;
+
+            // Define file path for the file to store the scores
+            string filePath = "record.txt";
+
+            MakeFile(filePath);
+
+            // Read all likes from record.txt
+            string[] lines = File.ReadAllLines(filePath);
+
+            int line1 = int.Parse(lines[0]); // Line1 for head
+            int line2 = int.Parse(lines[1]); // Line2 for tail
+
+            // Check if new score is higher than old scorefor heads, if is, then update old high score to the new one
+            if (line1 < HeadAmount)
+            {
+                NewHeadRecordLabel.Text = "You set a new record for consecutive heads: " + HeadAmount;
+                OldHeadRecordLabel.Text = "The record for consecutive heads was: " + line1;
+                lines[0] = HeadAmount.ToString();
+                File.WriteAllLines(filePath, lines);
+            } else
+            {
+                NewHeadRecordLabel.Text = "You did not set a new record for consecutive heads: " + HeadAmount;
+                OldHeadRecordLabel.Text = "The orecord for consecutive heads was: " + line1;
+            }
+
+            // Check if new score is higher than old score for tails, if is, then update old high score to the new one
+            if (line2 < TailAmount)
+            {
+                NewTailRecordLabel.Text = "You set a new record for consecutive tails: " + TailAmount;
+                OldTailRecordLabel.Text = "The old record for consecutive tails was: " + line2;
+                lines[1] = TailAmount.ToString();
+                File.WriteAllLines(filePath, lines);
+            } else
+            {
+                NewTailRecordLabel.Text = "You did not set a new record for consecutive tails: " + TailAmount;
+                OldTailRecordLabel.Text = "The old record for consecutive tails was: " + line2;
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
